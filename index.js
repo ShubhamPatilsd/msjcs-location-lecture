4cac56f2a3d87345f4f40be718bf0f497f8e5d0f
Shubham <shubhampatilsd@gmail.com>
2022-05-22 16:16:04 -0700

    codding

---
+++ index.js
@@ -0,0 +1,15 @@
+const response = fetch("https://ipapi.co/json/")
+  .then((data) => data.json())
+  .then((location) => {
+    console.log(location);
+
+    let map = L.map("map").setView([location.latitude, location.longitude], 10);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+      map
+    );
+    L.marker([location.latitude, location.longitude]).addTo(map);
+
+    document.getElementById(
+      "city"
+    ).textContent = `You live in ${location.city}, ${location.country_name}`;
+  });
